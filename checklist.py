a9d2eaf15966f953b5dd60754d49af0965d9bc4b
Luke <deandrevidal@aol.com>
2019-09-03 20:15:05 -0700

    trying again

--- checklist.py
+++ checklist.py
@@ -10,19 +10,19 @@ def create(item):
 
 
 def read(index):
-    return checklist[index]
+    print(checklist[int(index)])
 
 # update
 
 
 def update(index, item):
-    checklist[index] = item
+    checklist[int(index)] = str(item)
 
 # delete
 
 
 def destroy(index):
-    checklist.pop(index)
+    checklist.pop[int(index)]
 
 # testing
 
@@ -59,39 +59,7 @@ def mark_completed(index):
     checklist[int(index)] = "√ " + checklist[index]
 
 
-def select(function_code):
-    # Create item
-    if function_code == "C":
-        input_item = user_input("Input item: ")
-        create(input_item)
-
-    # Read item
-    elif function_code == "R":
-        item_index = user_input("Index Number? ")
-
-        # Remember that item_index must actually exist or our program will crash.
-        read(item_index)
-
-    # Print all items
-    elif function_code == "P":
-        list_all_items()
-    elif function_code == "Q":
-        # This is where we want to stop our loop
-        return False
-    # Catch all
-    else:
-        print("Unknown Option")
-
-
-def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
-    user_input = input(prompt)
-    return user_input
-
-
-test()
-
+# test()
 running = True
 while running:
     selection = user_input(
